0624090570e8ae2e539de31d368c414be4f8b77b
Nanda Gopal <ngopal253@gmail.com>
2020-03-10 14:55:21 +0530

    testing both client and server commits

---
+++ client/src/auth/Signin.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import Layout from '../core/Layout';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import { authenticate, isAuth } from './helpers';
+
+const Signin = () => {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    buttonText: 'Submit'
+  });
+
+  const { email, password, buttonText } = values;
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const clickSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, buttonText: 'Submitting' });
+    axios({
+      method: 'POST',
+      url: `${process.env.REACT_APP_API}/signin`,
+      data: { email, password }
+    })
+      .then(response => {
+        console.log('SIGNIN SUCCESS:- ', response);
+        setValues({
+          ...values,
+          buttonText: 'Submitted'
+        });
+        // save the response (user, token) in localStorage/cookie
+        // user information in localStorage and token in cookie
+
+        authenticate(response, () => {
+          toast.success(`Hey ${response.data.user.name}, Welcome back!`);
+          setTimeout(() => {
+            setValues({
+              ...values,
+              name: '',
+              email: '',
+              password: '',
+              buttonText: 'Submit'
+            });
+          }, 1000);
+        });
+      })
+      .catch(error => {
+        console.log('SIGNIN ERROR:- ', error.response.data);
+        setValues({ ...values, buttonText: 'Submit' });
+        toast.error(error.response.data.error);
+      });
+  };
+
+  const signinForm = () => (
+    <form>
+      <div className='form-group'>
+        <label className='text-muted'>Email</label>
+        <input
+          onChange={handleChange('email')}
+          type='email'
+          className='form-control'
+          value={email}
+        />
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Password</label>
+        <input
+          onChange={handleChange('password')}
+          type='password'
+          className='form-control'
+          value={password}
+        />
+      </div>
+      <div>
+        <button className='btn btn-primary' onClick={clickSubmit}>
+          {buttonText}
+        </button>
+      </div>
+    </form>
+  );
+
+  return (
+    <Layout>
+      {/* {JSON.stringify(isAuth())} */}
+      <div className='col-md-6 offset-md-3'>
+        <ToastContainer />
+        {isAuth() ? <Redirect to='/' /> : null}
+        {/* {JSON.stringify({ name, email, password })} */}
+        <h1 className='p-5 text-center'>Signin</h1>
+        {signinForm()}
+      </div>
+    </Layout>
+  );
+};
+
+export default Signin;
---
+++ server/routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+// import controllers
+const { signup, accountActivation, signin } = require('../controllers/auth');
+
+// import validators
+const {
+  userSignupValidator,
+  userSigninValidator
+} = require('../validators/auth'); //<-- array of checks
+const { runValidation } = require('../validators');
+
+router.post('/signup', userSignupValidator, runValidation, signup);
+router.post('/account-activation', accountActivation);
+router.post('/signin', userSigninValidator, runValidation, signin);
+
+module.exports = router;
